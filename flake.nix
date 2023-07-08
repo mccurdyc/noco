bd427b62683c15db01c10e88346d678eb7b14af1
Colton J. McCurdy <mccurdyc22@gmail.com>
2023-07-08 12:30:45 -0400

    simple nix shell
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,17 @@
 {
-  description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [pkgs.bashInteractive];
-        buildInputs = with pkgs; [
-          gnumake
-          google-cloud-sdk
-        ];
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells = {
+            default = import ./shell.nix { inherit pkgs; };
+          };
+        }
+      );
 }
